5a49de81d82aee6e705860bf1aa8b2c458d6f2a1
minh-quann <quannguyen19092003@gmail.com>
2025-05-27 10:55:43 +0700

    update frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <Header />
-  <router-view />
-  <Footer />
+  <div id="app">
+    <Header />
+    <div class="main-content">
+      <router-view /> 
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
-
 export default {
   components: { Header, Footer },
 };
 </script>
 
 <style>
-html, body {
+html,body,#app {
+  height: 100%;
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router/router.js' 
-
+import 'dayjs'
 const app = createApp(App)
 
 app.use(router)
