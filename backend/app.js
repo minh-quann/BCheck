91b63767a07d5472494620ba9284dd3564185723
Nguyễn Như Tú <tunguyennhuxyz@gmail.com>
2025-06-03 10:16:03 +0700

    Revert "users/registerUser"
    This reverts commit 3d405d65bfb8d474bc1b0b9eea25ebe42a919e72.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
-import express from "express";
-import syncModels from "./models/syncModel.js";
-import roleRouter from "./routers/roleRouter.js";
-import jobRouter from "./routers/jobRouter.js";
-import userRouter from "./routers/userRouter.js";
-import cors from "cors";
+import express from 'express';
+import syncModels from './models/syncModel.js';
+import roleRouter from './routers/roleRouter.js';
+import jobRouter from './routers/jobRouter.js';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
@@ -12,21 +11,20 @@ app.use(express.json());
 app.use(cors());
 
 // Định tuyến
-app.use("/api/roles", roleRouter);
+app.use('/api/roles', roleRouter);
 app.use("/api/jobs", jobRouter);
-app.use("/api/users", userRouter);
 
 // Đồng bộ model và khởi chạy server
 (async () => {
   try {
     await syncModels();
-    console.log(" Đã đồng bộ bảng thành công.");
+    console.log(' Đã đồng bộ bảng thành công.');
 
     app.listen(port, () => {
       console.log(` Server chạy tại: http://localhost:${port}`);
     });
   } catch (error) {
-    console.error(" Lỗi khi đồng bộ bảng:", error);
-    process.exit(1);
+    console.error(' Lỗi khi đồng bộ bảng:', error);
+    process.exit(1); 
   }
 })();
--- backend/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-import express from "express";
-import { registerUser } from "../controllers/userController.js"; // Assuming userController.js contains registerUser
-
-const router = express.Router();
-
-router.post("/register", registerUser);
-
-export default router;
