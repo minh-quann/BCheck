acdda4ab970181cf92fafc704ac19feed6e80a7c
Nguyễn Như Tú <tunguyennhuxyz@gmail.com>
2025-06-10 16:34:36 +0700

    update

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,34 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import Home from '../views/Home.vue'
-import UploadPanel from '../views/UploadPanel.vue'
+import Home from "../views/Home.vue";
+import UploadPanel from "../views/UploadPanel.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue')
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
   },
   {
-    path: '/upload',
-    name: 'DocumentForm',
-    component: () => import('@/components/DocumentForm.vue')
+    path: "/upload",
+    name: "DocumentForm",
+    component: () => import("@/components/DocumentForm.vue"),
   },
-  {
-    path: '/document',
-    name: 'DocumentList',
-    component: () => import('@/components/JobList.vue')
-  },
-  {
-    path: '/detail/:id',
-    name: 'JobDetail',
-    component: () => import('@/components/JobDetail.vue')
-  }
-]
+  // {
+  //   path: "/document",
+  //   name: "DocumentList",
+  //   component: () => import("@/components/JobList.vue"),
+  // },
+  // {
+  //   path: "/detail/:id",
+  //   name: "JobDetail",
+  //   component: () => import("@/components/JobDetail.vue"),
+  // },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
